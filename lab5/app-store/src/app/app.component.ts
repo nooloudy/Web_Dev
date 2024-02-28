032193fe28dccc7b7274eb99c80f792825f6697a
nooloudy <n_orynbassar@kbtu.kz>
2024-02-29 00:12:14 +0600

    added lab5

---
+++ lab5/app-store/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true
+})
+export class AppComponent {
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
