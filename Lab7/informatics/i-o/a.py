e3562da738fc960f4ac60d197107ee308a1e153e
nooloudy <n_orynbassar@kbtu.kz>
2024-03-24 20:07:34 +0600

    Django

---
+++ Lab7/informatics/i-o/a.py
@@ -0,0 +1,4 @@
+import math
+num1 = float(input())
+num2 = float(input())
+print(math.sqrt(num1**2 + num2**2))
---
+++ Lab7/informatics/i-o/e.py
@@ -0,0 +1,4 @@
+x = int(input())
+y = int(input())
+
+print(((x * y) % 109 + 109) % 109)
